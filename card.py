6c8a0c8d6d9f739a92b3a5d198ea5cc6494247e3
rumacalin <russell.macalintal@gmail.com>
2022-02-15 22:20:20 -0500

    Finished Deck Class definition

--- card.py
+++ card.py
@@ -1,5 +1,7 @@
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
 
 class Card:
@@ -12,7 +14,8 @@ class Card:
     def __str__(self):
         return self.rank + " of " + self.suit
 
-if __name__ == '__main__':
-    two_hearts = Card('Hearts', 'Two')
-    print(two_hearts)
-    print(f'Value: {two_hearts.value}')
+# Test Card Class functionality
+# if __name__ == '__main__':
+#     two_hearts = Card('Hearts', 'Two')
+#     print(two_hearts)
+#     print(f'Value: {two_hearts.value}')
---
+++ deck.py
@@ -0,0 +1,40 @@
+import card                         # Import card.py file
+import random                       # Import built-in Python random module
+
+class Deck:
+    
+    def __init__(self):
+        self.all_cards = []
+
+        for suit in card.suits:
+            for rank in card.ranks:
+                # Create the Card Object
+                created_card = card.Card(suit, rank)
+                # Add newly created Card Object to list
+                self.all_cards.append(created_card)
+
+    def shuffle(self):
+        # random.shuffle destructively affects the passed in arg. It does not return a new "shuffled" list
+        random.shuffle(self.all_cards)
+
+    def deal_one(self):
+        # Returns the last card removed from the list
+        return self.all_cards.pop()
+
+
+# Test Deck Class functionality
+if __name__ == '__main__':
+    # Create new Deck Object
+    new_deck = Deck()
+    # Show the last Card Object created and included into the Deck
+    top_card = new_deck.all_cards[-1]
+    print(f'The top card is {top_card}')         # Ace of Clubs
+
+    print('Shuffling...')
+    new_deck.shuffle()
+    top_card = new_deck.all_cards[-1]
+    print(f'The top card is now {top_card}') 
+
+    print('Dealing...')
+    my_card = new_deck.deal_one()
+    print(f'My card is {my_card}. The deck now has {len(new_deck.all_cards)} cards left')
