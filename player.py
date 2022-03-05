8cf45dd06b9b24206cb767540a2d1b885aaefef5
Russell M <russell.macalintal@gmail.com>
2022-03-05 01:56:52 -0500

    Add functionality to Player cards

--- player.py
+++ player.py
@@ -8,10 +8,15 @@ class Player:
         self.all_cards = []
 
     def remove_one(self):
-        pass
+        return self.all_cards.pop(0)
 
     def add_cards(self, new_cards):
-        pass
+        if type(new_cards) == type([]):
+            # Adds multiple cards to the player's hand
+            self.all_cards.extend(new_cards)
+        else:
+            # Adds single card to the player's hand
+            self.all_cards.append(new_cards)
 
     def __str__(self):              # Default print method for Player object
         return f'Player {self.name} has {len(self.all_cards)} cards.'
